f6be9b4ba125d32deaf76d80ed9aea2cf0de0e46
acorbat <agustin.corbat@ki.se>
2023-08-02 15:46:31 +0200

    Add tests for cols in CellClones

--- tests/test_cell_clones.py
+++ tests/test_cell_clones.py
@@ -1,3 +1,7 @@
+import pytest
+import pandas as pd
+
+from pandera.errors import SchemaError
 from lintrec.cell_clones import CellClones
 
 
@@ -6,3 +10,22 @@ class TestCellClones:
 
     def test_has_documentation(self):
         assert CellClones.__doc__ is not None
+
+    def test_df_with_wrong_column(self):
+        with pytest.raises(SchemaError):
+            df = pd.DataFrame(
+                [["a", "1"], ["a", "2"], ["b", "2"]],
+                columns=["cell_type", "cla"],
+            )
+            CellClones(df)
+
+    def test_df_missing_column(self):
+        with pytest.raises(SchemaError):
+            df = pd.DataFrame(["a", "a", "b"], columns=["cell_type"])
+            CellClones(df)
+
+    def test_ok_df(self):
+        df = pd.DataFrame(
+            [["a", "1"], ["a", "2"], ["b", "2"]], columns=["cell_type", "clone"]
+        )
+        CellClones(df)
