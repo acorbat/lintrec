d160e1e5cfdedf7a642136cd31d2e0cf85470812
acorbat <agustin.corbat@ki.se>
2023-08-02 14:49:27 +0200

    Add Doc for Cell Clones

--- lintrec/cell_clones.py
+++ lintrec/cell_clones.py
@@ -2,5 +2,8 @@ import pandera as pa
 
 
 class CellClones(pa.DataFrameModel):
+    """CellClones is a DataFrame where each row contains cell type and identity
+    of the clone of each cell."""
+
     cell_type: str
     clone: str
