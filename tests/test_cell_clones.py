7fd27154b92b8f841b3755c166e85e0f3228f84f
acorbat <agustin.corbat@ki.se>
2023-08-03 11:21:24 +0200

    Add initial structure and CellClones class

---
+++ tests/test_cell_clones.py
@@ -0,0 +1,41 @@
+import pytest
+import pandas as pd
+
+from pandera.errors import SchemaError
+from lintrec.cell_clones import CellClones
+
+
+class TestCellClones:
+    """Test that CellClones class behaves as expected."""
+
+    def test_has_documentation(self):
+        assert CellClones.__doc__ is not None
+
+    def test_df_with_wrong_column(self):
+        with pytest.raises(SchemaError):
+            df = pd.DataFrame(
+                [["a", "1"], ["a", "2"], ["b", "2"]],
+                columns=["cell_type", "cla"],
+            )
+            CellClones(df)
+
+    def test_df_missing_column(self):
+        with pytest.raises(SchemaError):
+            df = pd.DataFrame(["a", "a", "b"], columns=["cell_type"])
+            CellClones(df)
+
+    def test_ok_df(self):
+        df = pd.DataFrame(
+            [["a", "1"], ["a", "2"], ["b", "2"]],
+            columns=["cell_type", "clone"],
+            dtype="category",
+        )
+        CellClones(df)
+
+    def test_ok_df_with_numbers(self):
+        df = pd.DataFrame(
+            [["a", 1], ["a", 2], ["b", 2]],
+            columns=["cell_type", "clone"],
+            dtype="category",
+        )
+        CellClones(df)
